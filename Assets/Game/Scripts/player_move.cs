1944e53146f33e563e52a7c8f731b8afeac55e8c
lhz <2433924799@qq.com>
2022-12-17 16:52:37 +0800

    basic movement completed

---
+++ Assets/Game/Scripts/player_move.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController cc;
+    private float horizontal, vertical;
+    public float move_speed;
+    private Vector3 dir;
+    public float gravity;
+   private float velocity_vertical;
+    private void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+    }
+
+    void PlayerMovement()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        dir.Set(horizontal, 0, vertical);
+        dir = Quaternion.Euler(0, -45f, 0) * dir; 
+        if(dir!=Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(dir);
+        }
+        
+        if (cc.isGrounded == false) 
+        {
+            velocity_vertical = gravity;  //为防止竖直速度无限增大，直接给定一个恒定速度
+        }
+        else
+        {
+            velocity_vertical = gravity * 0.3f;
+        }
+        dir *= move_speed * Time.deltaTime;
+        dir += Vector3.up * velocity_vertical;
+       
+        cc.Move(dir);
+    }
+}
