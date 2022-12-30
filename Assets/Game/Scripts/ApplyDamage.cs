773f7054c938b1c3bffa16aec00fc4e0e278b044
lhz <2433924799@qq.com>
2022-12-30 08:55:29 +0800

    Slash completed

--- Assets/Game/Scripts/ApplyDamage.cs
+++ Assets/Game/Scripts/ApplyDamage.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class ApplyDamage : MonoBehaviour
 {
@@ -9,19 +10,28 @@ public class ApplyDamage : MonoBehaviour
     public List<Collider> damageTargets;
     public string TargetTag;
     public float Damage;
+   
     private void Awake()
     {
         damageTargets = new List<Collider>();
         _collider.enabled = false;
     }
 
+   
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag==TargetTag&&!damageTargets.Contains(other))
         {
+          
             Health health_other = other.GetComponent<Health>();
             health_other.ApplyDamage(Damage);
             damageTargets.Add(other);
+            PlayerVFXmanager playerVFXmanager = transform.parent.GetComponent<PlayerVFXmanager>();
+            if(playerVFXmanager!=null)
+            {
+                playerVFXmanager.PlaySlash(other.transform.position);
+                
+            }
         }
     }
 
--- Assets/Game/Scripts/PlayerVFXmanager.cs
+++ Assets/Game/Scripts/PlayerVFXmanager.cs
@@ -8,6 +8,7 @@ public class PlayerVFXmanager : MonoBehaviour
 {
     public VisualEffect footstep;
     public ParticleSystem blade_01;
+    public VisualEffect Slash;
     public void update_footstep(bool state)
     {
         if(state)
@@ -23,6 +24,12 @@ public class PlayerVFXmanager : MonoBehaviour
     public void PlayBlade_01()
     {
         blade_01.Play();
+        
     }
 
+    public void PlaySlash(Vector3 pos)
+    {
+        Slash.transform.position = pos;
+        Slash.Play();
+    }
 }
